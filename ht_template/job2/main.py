5c25166888477afe960f70192ec6dc448c6b7306
anatoliidomanskii <anatoliidomanskii@gmail.com>
2022-09-05 22:53:18 +0300

    v0.2.0 Second program has done.

--- ht_template/job2/main.py
+++ ht_template/job2/main.py
@@ -1,12 +1,36 @@
 from flask import Flask, request
-
+from fastavro import parse_schema, writer
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def main():
-    return "111"
+
+    raw_dir = request.headers['raw_dir']
+    stg_dir = request.headers['stg_dir']
+
+    schema = {
+        'name': 'Sales',
+        'type': 'record',
+        'fields': [
+            {'name': 'client', 'type': 'string'},
+            {'name': 'purchase_date', 'type': 'string'},
+            {'name': 'product', 'type': 'string'},
+            {'name': 'price', 'type': 'int'}
+        ],
+    }
+    parsed_schema = parse_schema(schema)
+
+    with open(raw_dir + '.json', 'r') as file_in:
+        records = json.load(file_in)
+        print(records)
+        with open(stg_dir + '.avro', 'wb') as out:
+            print(out)
+            writer(out, parsed_schema, records)
+
+    return 'Done!'
 
 
 if __name__ == "__main__":
--- ht_template/job2/user.py
+++ ht_template/job2/user.py
@@ -3,10 +3,10 @@ import requests
 
 res = requests.post(url='http://localhost:8082/',
                     headers={
-                        "raw_dir": "/path/to/my_dir/raw/sales/2022-08-09",
-                        "stg_dir": "/path/to/my_dir/stg/sales/2022-08-09"})
+                        "raw_dir": "D:/robot_dreams/1/lect_02/ht_template/job1/path/to/my_dir/raw/sales/2022-08-09",
+                        "stg_dir": "D:/robot_dreams/1/lect_02/ht_template/job1/path/to/my_dir/stg/sales/2022-08-09"})
 
 if res.ok:
-    print(res.json())
+    print(res.content)
 else:
     print(res.status_code)
